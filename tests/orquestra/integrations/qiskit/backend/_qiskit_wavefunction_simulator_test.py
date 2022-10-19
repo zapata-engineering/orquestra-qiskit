5c20695ca3c27650713cf30ae08dd64a59d41b5f
Konrad Jałowiecki <dexter2206@gmail.com>
2022-10-19 12:56:01 +0200

    refactor: reorganize fixtures

--- tests/orquestra/integrations/qiskit/backend/_qiskit_wavefunction_simulator_test.py
+++ tests/orquestra/integrations/qiskit/backend/_qiskit_wavefunction_simulator_test.py
@@ -5,19 +5,18 @@ from orquestra.integrations.qiskit.backend import QiskitWavefunctionSimulator
 from orquestra.quantum.api.circuit_runner_contracts import CIRCUIT_RUNNER_CONTRACTS
 from orquestra.quantum.api.wavefunction_simulator_contracts import simulator_contracts_for_tolerance
 
-
-COMPATIBLE_BACKENDS = ["aer_simulator_statevector", "statevector_simulator"]
+STATEVECTOR_BACKENDS = ["aer_simulator_statevector", "statevector_simulator"]
 
 
 def _test_id(val):
     return val.backend.name()
 
 
-@pytest.mark.parametrize(
-    "simulator",
-    [QiskitWavefunctionSimulator(Aer.get_backend(name)) for name in COMPATIBLE_BACKENDS],
-    ids=_test_id,
-)
+@pytest.fixture(params=STATEVECTOR_BACKENDS)
+def simulator(request):
+    return QiskitWavefunctionSimulator(Aer.get_backend(request.param))
+
+
 @pytest.mark.parametrize("contract", CIRCUIT_RUNNER_CONTRACTS)
 def test_qiskit_wavefucntion_simulator_fulfills_circuit_runner_contracts(
     simulator, contract
@@ -25,11 +24,6 @@ def test_qiskit_wavefucntion_simulator_fulfills_circuit_runner_contracts(
     assert contract(simulator)
 
 
-@pytest.mark.parametrize(
-    "simulator",
-    [QiskitWavefunctionSimulator(Aer.get_backend(name)) for name in COMPATIBLE_BACKENDS],
-    ids=_test_id,
-)
 @pytest.mark.parametrize("contract", simulator_contracts_for_tolerance())
 def test_qiskit_wf_simulator_fulfills_wf_simulator_contracts(
     simulator, contract
