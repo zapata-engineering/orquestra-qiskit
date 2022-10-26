85988094a3f0231d95b543ba845245ef8eb87d57
Konrad Jałowiecki <dexter2206@gmail.com>
2022-10-26 14:17:27 +0200

    feat: remove old backend and simulator

--- src/orquestra/integrations/qiskit/backend/__init__.py
+++ src/orquestra/integrations/qiskit/backend/__init__.py
@@ -1,6 +1,5 @@
 ################################################################################
 # © Copyright 2020-2022 Zapata Computing Inc.
 ################################################################################
-from .backend import QiskitBackend
 from ._qiskit_runner import QiskitRunner
 from ._qiskit_wavefunction_simulator import QiskitWavefunctionSimulator
