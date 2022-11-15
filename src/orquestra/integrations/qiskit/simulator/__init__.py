8c9798c4ab4534095bb1b1ee09377b8ac7a73849
Konrad Jałowiecki <dexter2206@gmail.com>
2022-11-15 16:18:10 +0100

    feat: move runner to separate subpackage

--- src/orquestra/integrations/qiskit/simulator/__init__.py
+++ src/orquestra/integrations/qiskit/simulator/__init__.py
@@ -1,5 +1,4 @@
 ################################################################################
 # © Copyright 2020-2022 Zapata Computing Inc.
 ################################################################################
-from ._qiskit_runner import QiskitRunner
-from ._ibmq_runner import create_ibmq_runner
+from ._qiskit_wavefunction_simulator import QiskitWavefunctionSimulator
